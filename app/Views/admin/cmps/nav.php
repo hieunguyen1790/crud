1d8d9a652bbd944e0388cd9c7e055dd6ecb96de8
hieunguyen1790 <69892497+hieunguyen1790@users.noreply.github.com>
2020-08-26 18:38:41 +0700

    Update lan nn

--- app/Views/admin/cmps/nav.php
+++ app/Views/admin/cmps/nav.php
@@ -18,7 +18,7 @@
                 <i class="nav-icon fas fa-project-diagram"></i>
                 <p>Projects</p>
             </a>
-        </li>
+        </li> 
         <li class="nav-item">
             <a href="<?= base_url();?>/blog" class="nav-link <?= $request->uri->getSegment(1) == 'blog' ? 'active' : null; ?>">
                 <i class="nav-icon fas fa-list-alt"></i><p>Blog</p>
